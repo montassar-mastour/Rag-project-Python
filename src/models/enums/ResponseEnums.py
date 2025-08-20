b8025e195eca7053684db67a475659ce76dea958
mastour-montassar <montassar.mastour@isamm.u-manouba.tn>
2025-08-20 15:20:53 +0100

    create the uploading file system

---
+++ src/models/enums/ResponseEnums.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class ResponseMessage(Enum):
+
+
+
+    FILE_VALIDATE_SUCCESS="File Validate successfuly"
+    FILE_Type_NOT_SUPPORTED= "File Type Not Supported"
+    FILE_Size_TOO_BIG="File Size Too Big"
+    UPLOAD_WITH_SUCCES="Upload With succes"
+    UPLOAD_fAILED="Upload_Upload_failed"
---
+++ src/routes/data.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI, APIRouter, Depends, UploadFile, status
+from fastapi.responses import JSONResponse
+from helpers.config import get_settings,settings
+from controllers import DataController, ProjectController
+import os,aiofiles, logging
+from models import ResponseMessage
+
+logger=logging.getLogger('uvicorn.error')
+
+data_router = APIRouter(
+     prefix="/api/v1/data",
+     tags=["api_v1","data"],
+
+)
+
+
+@data_router.post("/upload/{project_id}")
+async def welcome(project_id: str,file: UploadFile, 
+                  app_settings : settings = Depends(get_settings) ):
+    
+    data_controller =DataController()
+
+    is_valid, result_message = data_controller.validate_upload_file(file=file)
+
+    if not is_valid:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "message:" : result_message
+            }
+        )
+
+    file_path = data_controller.generate_unique_filename(
+        orig_filename=file.filename,
+        project_id=project_id
+    )
+
+    try:
+        async with aiofiles.open(file_path, "wb") as f:
+            while chunk := await file.read(app_settings.FILE_DEFAULT_CHUNK_SIZE):
+                await f.write(chunk)
+    except Exception as e:
+        logger.error(f"Error while uploading file:  {e}")
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                "message:" : ResponseMessage.UPLOAD_fAILED.value
+            }
+        )
+            
+
+    return JSONResponse(
+    content={
+        "message:" : ResponseMessage.UPLOAD_WITH_SUCCES.value
+    }
+)
+
+
+
+
+
+
