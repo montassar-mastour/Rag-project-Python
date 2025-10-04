a929fff8c4bbf028e3d33f2c75220487bafdd2ba
mastour-montassar <montassar.mastour@isamm.u-manouba.tn>
2025-10-04 11:44:47 +0100

    create  and implement the factory provider for llms

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -12,6 +12,21 @@ class settings(BaseSettings):
     MONGO_URL: str
     MONGO_DB_NAME: str
 
+    GENERATION_BACKEND : str
+    EMBEDDING_BACKEND : str
+
+    OPENAI_API_KEY : str = None
+    OPENAI_API_URL : str =  None
+    COHERE_API_KEY : str =  None
+
+    GENERATION_MODEL_ID : str =  None
+    EMBEDDING_MODEL_ID : str =  None
+    EMBEDDING_MODEL_SIZE : int =  None
+
+    INPUT_DEFAULT_MAX_CARACTERS : int =  None
+    GENERATION_DEFAULT_MAX_TOKENS : int =  None
+    GENERATION_DEFAULT_TEMPERATURE : float =  None
+
     class Config:
         env_file = ".env"
 
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from fastapi.concurrency import asynccontextmanager
 from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 @asynccontextmanager
 
@@ -12,6 +13,15 @@ async def lifespan(app: FastAPI):
     app.mongodb_client = AsyncIOMotorClient(settings.MONGO_URL)
     app.db_client = app.mongodb_client[settings.MONGO_DB_NAME]
     print("Connected to the MongoDB database")
+    llm_provider_factory = LLMProviderFactory(settings)
+
+    #generation client
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND) 
+    app.generation_client.set_generation_model(model_id= settings.GENERATION_MODEL_ID)
+
+    #embedding client
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id= settings.EMBEDDING_MODEL_ID, embedding_size=settings.EMBEDDING_MODEL_SIZE)
 
     yield
 
