b3634a89e2fb7e041cb4816983257b5dc368247a
mastour-montassar <montassar.mastour@isamm.u-manouba.tn>
2025-10-05 12:13:52 +0100

    add the configuration of DB Vector

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -27,6 +27,11 @@ class settings(BaseSettings):
     GENERATION_DEFAULT_MAX_TOKENS : int =  None
     GENERATION_DEFAULT_TEMPERATURE : float =  None
 
+
+    VECTOR_DB_BACKEND : str
+    VECTOR_DB_PATH : str
+    VECTOR_DB_DISTANCE_METHOD :str = None
+
     class Config:
         env_file = ".env"
 
