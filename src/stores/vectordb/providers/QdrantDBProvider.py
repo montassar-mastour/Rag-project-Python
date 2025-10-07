8dd871bd2b4a83ac7ca093bd7b2cd3ce173e78a7
mastour-montassar <montassar.mastour@isamm.u-manouba.tn>
2025-10-07 23:57:52 +0100

    update Qdrantdb provider with record_id

--- src/stores/vectordb/providers/QdrantDBProvider.py
+++ src/stores/vectordb/providers/QdrantDBProvider.py
@@ -66,6 +66,7 @@ class QdrantDBProvider(VectorDBInterface):
                 collection_name=collection_name,
                 records=[  
                     models.Record(
+                        id=[record_id],
                         vector=vector,
                         payload={"text": text, "metadata":metadata
                         }
@@ -83,7 +84,7 @@ class QdrantDBProvider(VectorDBInterface):
             metadata =[]*len(texts)
 
         if record_ids == None:
-            record_ids = [None]*len(texts)
+            record_ids = list(range(0, len(texts)))
 
         for i in range(0, len(texts), batch_size):
             batch_end = i + batch_size
@@ -91,9 +92,11 @@ class QdrantDBProvider(VectorDBInterface):
             batch_texts = texts[i:batch_end]
             batch_vectors = vectors[i:batch_end]
             batch_metadata = metadata[i:batch_end]
+            batch_records_ids = record_ids[i:batch_end]
 
             batch_records=[
                 models.Record(
+                    id=batch_records_ids[x],
                     vector=batch_vectors[x],
                     payload={"text": batch_texts[x], "metadata":batch_metadata[x]
                     }
