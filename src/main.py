4b4399bb3abbd23ac12048cbe37549a1e6b902f1
mastour-montassar <montassar.mastour@isamm.u-manouba.tn>
2025-10-08 00:29:56 +0100

    update main with  adding Vectordb provider factory  and  NLP route

--- src/main.py
+++ src/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.concurrency import asynccontextmanager
-from routes import base, data
+from routes import base, data, nlp
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
 @asynccontextmanager
 
@@ -14,24 +15,32 @@ async def lifespan(app: FastAPI):
     app.db_client = app.mongodb_client[settings.MONGO_DB_NAME]
     print("Connected to the MongoDB database")
     llm_provider_factory = LLMProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(settings)
 
     #generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND) 
-    app.generation_client.set_generation_model(model_id= settings.GENERATION_MODEL_ID)
+    app.generation_client.set_generate_method(model_id= settings.GENERATION_MODEL_ID)
 
     #embedding client
     app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
     app.embedding_client.set_embedding_model(model_id= settings.EMBEDDING_MODEL_ID, embedding_size=settings.EMBEDDING_MODEL_SIZE)
+
+    #vectorDB client
+    app.vectordb_client = vectordb_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
+    app.vectordb_client.connect()
 
     yield
 
     # Shutdown code
     app.mongodb_client.close()
     print("Closed connection to the MongoDB database")
+    app.vectordb_client.disconnect()
 
 app =  FastAPI(lifespan=lifespan)
 
 app.include_router(base.base_router)
 
 app.include_router(data.data_router)
+
+app.include_router(nlp.nlp_router)
 
