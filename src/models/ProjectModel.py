5a09d930164b36d1d7e25807814f31ae26ea5985
mastour-montassar <montassar.mastour@isamm.u-manouba.tn>
2025-10-20 23:34:15 +0100

    change the models to work with sqlalchemy instead of mongodb

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -1,6 +1,8 @@
 from .BaseDataModel import BaseDataModel
 from .db_schemes import Project
 from .enums.DataBaseEnum import DataBaseEnum as Da
+from sqlalchemy.future import select
+from sqlalchemy import func
 
 class ProjectModel(BaseDataModel):
     """
@@ -10,64 +12,59 @@ class ProjectModel(BaseDataModel):
 
     def __init__(self, db_client: object):
         super().__init__(db_client=db_client)
-        self.collection = self.db_client[Da.COLLECTION_PROJECT_NAME.value]
+        self.db_client = db_client
 
     @classmethod    
     async def create_instances(cls, db_client: object):
         instance = cls(db_client=db_client)
-        await instance.init_collection_indexes()
         return instance
     
 
 
-    async def init_collection_indexes(self):
-        all_collections = await self.db_client.list_collection_names()
-        if Da.COLLECTION_PROJECT_NAME.value not in all_collections:
-            self.collection = self.db_client[Da.COLLECTION_PROJECT_NAME.value]
-            indexes = Project.get_indexes()
-            for index in indexes:
-                await self.collection.create_index(
-                    index["keys"],
-                    name=index["name"],
-                    unique=index["unique"]
-                )
-
-
-
     async def create_project(self, project: Project) :
+        async with self.db_client() as session:
+            async with session.begin():
+                session.add(project)
+            await session.commit()
+            await session.refresh(project)
 
-        doc = project.model_dump(by_alias=True, exclude_unset=True)
-        result = await self.collection.insert_one(doc)
-        project.id = result.inserted_id
-        return project
+        return project 
     
 
     async def get_project_or_create_one(self, project_id: str):
-        """
-        Retrieves a project by its ID or creates a new one if it doesn't exist.
-        """
-        record = await self.collection.find_one({"project_id": project_id})
+        async with self.db_client() as session:
+            async with session.begin():
+                query = select(Project).where(Project.project_id==project_id)
+                result = await session.execute(query)
+                project= result.scalar_one_or_none()
 
-        if record is not None:
-            return Project(**record)
-        
-        else:
-            project = Project(project_id=project_id)
-            project = await self.create_project(project=project)
-            return project
+                if project is None :
+                    project_recor= Project(
+                        project_id=project_id
+                    )
+                    project= await self.create_project(project=project_recor)
+                    return project
+                return project
+
+
         
     async def get_all_projects(self,page: int = 1, page_size: int = 10):
-        """
-        Retrieves all projects from the database.
-        """
+        async with self.db_client() as session:
+            async with session.begin():
 
-        total_doc = await self.collection.count_documents({})
-        total_pages = (total_doc + page_size - 1) // page_size 
-        skip = (page - 1) * page_size
-        curser= self.collection.find().skip(skip).limit(page_size)
-        projects = [Project(**doc) async for doc in curser]
+                total_doc=  await session.execute(select(
+                    func.count(Project.project_id)
+                )).scalar_one()
 
-        return projects, total_pages
+                total_page=(total_doc + page_size - 1) // page_size 
+                skip = (page - 1) * page_size
+
+                querry = select(Project).offset(skip).limit(page_size)
+                results = await session.execute(querry)
+                projects= results.scalars().all()
+
+                return projects, total_page
+
 
 
 
