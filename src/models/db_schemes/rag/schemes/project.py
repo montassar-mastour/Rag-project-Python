9a79be1c8fc129fbe68348dcd57ae0498c69a74b
mastour-montassar <montassar.mastour@isamm.u-manouba.tn>
2025-10-20 23:28:30 +0100

    corrections  in schemes

--- src/models/db_schemes/rag/schemes/project.py
+++ src/models/db_schemes/rag/schemes/project.py
@@ -1,6 +1,7 @@
 from .rag_base import SQLAlchemyBase
 from sqlalchemy import Column, Integer, DateTime, func
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 import uuid
 
 class Project(SQLAlchemyBase):
@@ -12,3 +13,6 @@ class Project(SQLAlchemyBase):
 
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
     updated_at = Column(DateTime(timezone=True), onupdate=func.now(), nullable=True)
+
+    chunks = relationship("DataChunk", back_populates="project")    
+    assets = relationship("Asset", back_populates="project")
