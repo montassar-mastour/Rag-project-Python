28f7e6d2de87c2518293911e281b1ca7918a43a9
mastour-montassar <montassar.mastour@isamm.u-manouba.tn>
2025-10-23 00:11:09 +0100

    update main to work with updated vectordb provider factory

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ async def lifespan(app: FastAPI):
     print("Connected to the Postgres database")
 
     llm_provider_factory = LLMProviderFactory(settings)
-    vectordb_provider_factory = VectorDBProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(config=settings, db_client=app.db_client)
 
     #generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND) 
@@ -35,7 +35,7 @@ async def lifespan(app: FastAPI):
 
     #vectorDB client
     app.vectordb_client = vectordb_provider_factory.create(provider=settings.VECTOR_DB_BACKEND)
-    app.vectordb_client.connect()
+    await app.vectordb_client.connect()
 
     app.template_parser = Template_Parser(
         language= settings.PRIMARY_LANG,
@@ -45,8 +45,7 @@ async def lifespan(app: FastAPI):
     yield
 
     # Shutdown code
-    app.db_engine.dispose()
-    print("Closed connection to the MongoDB database")
+    await app.db_engine.dispose()
     app.vectordb_client.disconnect()
 
 app =  FastAPI(lifespan=lifespan)
